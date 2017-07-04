bf765630c52d29442392efeaf57556a10b83704f
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-07-04 16:49:45 -0400

    WIP: styling nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <!-- Nav Section -->
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">Logo</a>
+        </div>
+
+      <!-- Hamburger Toggle -->
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+
+        <!-- Links using Vue's router link -->
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="/faq" class="nav-item r-item">FAQ</router-link>
+          <router-link to="/" class="nav-item r-item">About</router-link>
+          <router-link to="/faq" class="nav-item r-item">Contact</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now!</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
     <router-view></router-view>
   </div>
 </template>
@@ -16,14 +54,22 @@ export default {
 @import "../node_modules/bulma/bulma.sass"
 @import "mediaQuery"
 
+.nav
+  background-color: #383838
+  a:hover
+    color: $primary
 
+.nav-left a
+  color: #fff
+  font-weight: bold
 
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #F1F1F1
+      
 
 </style>
